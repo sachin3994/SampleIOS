ce72378b3c5bab6219467a938b93609bcf79398f
sachin3994 <s.rajput67@yahoo.com>
2017-07-18 15:23:14 -0700

    Hello world IOS
    Sample IOS

---
+++ Sample IOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample IOS
+//
+//  Created by MacStudent on 2017-07-18.
+//  Copyright © 2017 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
